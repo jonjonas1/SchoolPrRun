492d300723e49efd47dc04b1a9505e9314b21c91
CyberOJ <cyberOJ12@gmail.com>
2018-06-09 16:37:59 -0500

    MyMarhcJavaFiles

---
+++ src/March08/Mar8.java
@@ -0,0 +1,17 @@
+package March08;
+
+public class Mar8 {
+
+	public static void main(String[] args) {
+		
+		int x=4;
+		int y=2;
+		
+		int c= x * (y++) ;
+		{
+		System.out.println(y);
+		}
+		
+	}
+
+}
